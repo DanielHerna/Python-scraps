e2efca18c0bb6fa1fc39902944029f59145838f6
Daniel Hernandez <82115133+DanielHerna@users.noreply.github.com>
2026-02-17 08:11:58 +0800

    Add files via upload
    adding updates to Dealer class including Top up function and fixed bugs

--- 15OOP_challenge.py
+++ 15OOP_challenge.py
@@ -22,20 +22,25 @@ class Cars:
             print(f"car {self.plate} returned to the dealer")
 
 class User:
-    def __init__(self, balance,uid):
+    def __init__(self, uid, balance):
         self.uid = uid
         self.balance = balance
         self.rented_cars = []
 
     
-    def rent_user(self, car, balance):
-        if car.available and balance >= car.price:
-            car.rent_car()
-            self.rented_cars.append(car)
-            self.balance -= car.price
-            print(f"Thanks for your purchase, your new balance is {self.balance}")
+    def rent_user(self, car):
+        if car.available:
+            if self.balance >= car.price:
+                car.rent_car()
+                self.rented_cars.append(car)
+                self.balance -= car.price
+                print(f"Thanks for your purchase, your new balance is {self.balance}")
+            
+            else: 
+                print("ERROR: Insuficient balance, please top up your account")
+                
         else:
-            print("Insuficient balance, please top up your account")
+            print(f"ERROR: the car {car.plate} is not available")
 
     
     def ret_user(self, car):
@@ -44,11 +49,11 @@ class User:
             self.rented_cars.remove(car)
             print("Thanks for use our services")
         else:
-            print(f"user {self.uid} has not rented the car {car.plate}")
+            print(f"ERROR: user {self.uid} has not rented the car {car.plate}")
 
 
 class Dealer:
-    def __init__(self, users, cars):
+    def __init__(self):
         self.users = []
         self.cars = {}
 
@@ -59,13 +64,46 @@ class Dealer:
             print(f"{user.uid} added")
         
         else:
-            print(f"{user.uid} already exist")
+            print(f"ERROR: user {user.uid} already exist")
 
-
-    def add_car(self,car):
+    
+    def add_car(self, car):
         if car.plate not in self.cars:
             self.cars[car.plate] = car
-            print(f"{car.plate} added")
-        else:
-            print(f"Error, the car {car.plate} already exist")
+            print(f"the car {car.plate} was added")
+        
+        else: 
+            print(f"ERROR: The car {car.plate} already exist")
 
+
+    def top_up_user(self, user, top_up):
+        if user in self.users:
+            user.balance += top_up
+            print(f"The new balance for user {user.uid} is {user.balance}")
+        else:
+            print("ERROR: User not found")
+    
+
+    def show_users(self):
+        return [user.uid for user in self.users] 
+    
+
+    def show_cars(self):
+       return [car for car in self.cars]
+
+CAR_1 = Cars("Toyota","BLA979",500)
+CAR_2 = Cars("Mazda","AGT007",350)
+USER_1 = User(282930,5000)
+USER_2 = User(100007,2000)
+DEALER = Dealer()
+
+DEALER.add_user(USER_1)
+DEALER.add_user(USER_2)
+DEALER.add_car(CAR_1)
+DEALER.add_car(CAR_2)
+print(DEALER.show_users())
+print(DEALER.show_cars())
+
+DEALER.top_up_user(USER_1,3000)
+
+USER_1.rent_user(CAR_1)
